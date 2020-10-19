98f62034010d5163e1093d3b50657e9c810c75f7
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2020-10-19 13:00:14 -0600

    data types

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from the IntelliJ IDE!");
+
+        int studentAge = 15;
+        double someNumber = 23.4;
+        char firstLetter = 'j';
+        boolean hasPrefectAttendance = true;
+
+
+        System.out.println(studentAge);
+        System.out.println(someNumber);
+        System.out.println(firstLetter);
+        System.out.println(hasPrefectAttendance);
     }
 
 }
