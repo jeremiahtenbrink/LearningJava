4585511b4db9e3762339355cd09c4a72331ce8fd
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2020-10-19 13:03:28 -0600

    strings

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ public class Main {
         double someNumber = 23.4;
         char firstLetter = 'j';
         boolean hasPrefectAttendance = true;
+        String firstName = "Jeremiah";
+        String lastName = "Tenbrink";
 
 
         System.out.println(studentAge);
         System.out.println(someNumber);
         System.out.println(firstLetter);
         System.out.println(hasPrefectAttendance);
+        System.out.println(firstName);
+        System.out.println(lastName);
     }
 
 }
